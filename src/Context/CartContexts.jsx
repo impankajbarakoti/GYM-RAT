ab747c0bf29b60f1ffea3bf6935b843c685c1aec
Pankaj Barakoti <pankajbarakoti837@gmail.com>
2025-11-07 11:51:20 +0530

    newly

--- src/Context/CartContexts.jsx
+++ src/Context/CartContexts.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState } from "react";
 
-const CartContext = createContext();
+export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
@@ -57,5 +57,3 @@ export const CartProvider = ({ children }) => {
     </CartContext.Provider>
   );
 };
-
-export const useCart = () => useContext(CartContext);
